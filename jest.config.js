6138eb218392d224b6b3424dcc870f2b169b2f9d
Valentin Ichkour <valentin.ichkour@epitech.eu>
2020-07-25 15:24:49 +0200

    Initial commit

--- jest.config.js
+++ jest.config.js
@@ -1,56 +1,21 @@
 module.exports = {
-  globals: {
-    __DEV__: true,
-  },
-  setupFilesAfterEnv: ['<rootDir>/test/jest/jest.setup.js'],
-  // noStackTrace: true,
-  // bail: true,
-  // cache: false,
-  // verbose: true,
-  // watch: true,
-  collectCoverage: true,
-  coverageDirectory: '<rootDir>/test/jest/coverage',
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.vue',
-    '<rootDir>/src/**/*.js',
-    '<rootDir>/src/**/*.ts',
-    '<rootDir>/src/**/*.jsx',
-  ],
-  coverageThreshold: {
-    global: {
-      //  branches: 50,
-      //  functions: 50,
-      //  lines: 50,
-      //  statements: 50
-    },
-  },
-  testMatch: [
-    '<rootDir>/test/jest/__tests__/**/*.spec.js',
-    '<rootDir>/test/jest/__tests__/**/*.test.js',
-    '<rootDir>/src/**/__tests__/*_jest.spec.js',
-  ],
-  moduleFileExtensions: ['vue', 'js', 'jsx', 'json', 'ts', 'tsx'],
   moduleNameMapper: {
-    '^vue$': '<rootDir>/node_modules/vue/dist/vue.common.js',
-    '^test-utils$':
-      '<rootDir>/node_modules/@vue/test-utils/dist/vue-test-utils.js',
-    '^quasar$': '<rootDir>/node_modules/quasar/dist/quasar.common.js',
+    '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
-    '^src/(.*)$': '<rootDir>/src/$1',
-    '.*css$': '<rootDir>/test/jest/utils/stub.css',
+    '^vue$': 'vue/dist/vue.common.js'
   },
+  moduleFileExtensions: [
+    'js',
+    'vue',
+    'json'
+  ],
   transform: {
-    '.*\\.vue$': 'vue-jest',
-    '.*\\.js$': 'babel-jest',
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
-    // use these if NPM is being flaky
-    // '.*\\.vue$': '<rootDir>/node_modules/@quasar/quasar-app-extension-testing-unit-jest/node_modules/vue-jest',
-    // '.*\\.js$': '<rootDir>/node_modules/@quasar/quasar-app-extension-testing-unit-jest/node_modules/babel-jest'
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
   },
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!quasar/lang)'],
-  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
-  testPathIgnorePatterns: ['/node_modules/', '/src-cordova/', '/src/boot/'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/src-cordova/', '/src/boot/'],
-  watchPathIgnorePatterns: ['/node_modules/', '/src-cordova/', '/src/boot/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
+  ]
 }
