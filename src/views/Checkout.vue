d7a676bfa93d07f12002ac00d04227a7be726442
Valentin Ichkour <valentin.ichkour@getmayday.co>
2020-09-23 00:16:55 +0200

    fix redirection

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -8,40 +8,41 @@
 export default {
   name: "Checkout",
   mounted() {
-    if (this.$route.query.success === 'true') {
-      this.createServer()
-    } else if (this.$route.query.canceled === 'true') {
+    if (this.$route.query.success === "true") {
+      this.createServer();
+    } else if (this.$route.query.canceled === "true") {
       this.$notify({
         type: "danger",
-        title: "Couldn't create the server, please try again"
-      })
-      this.$router.push('/')
-      this.$store.commit('setCreateServerInfo', null)
+        title: "Couldn't create the server, please try again",
+      });
+      this.$router.push("/");
+      this.$store.commit("setCreateServerInfo", null);
     }
   },
   methods: {
     createServer() {
-      this.$store.commit('setCreateServerInfo', null)
       this.$store.state.client.Docker.create(
-          this.$store.state.user.ID.toString(),
-          "minecraft",
-          this.$store.state.serverCreateInfo.name
-        )
-          .then((response) => {
-            console.log(response);
-            this.$router.push({ path: "/dashboard" });
-            this.$notify({
-              type: "success",
-              title: `Server ${this.serverName} correctly created`,
-            });
-          })
-          .catch((e) => {
-            this.$notify({
-              type: "danger",
-              title: `Something went wrong: ${e._message.message}`,
-            });
+        this.$store.state.user.ID.toString(),
+        "minecraft",
+        this.$store.state.serverCreateInfo.name
+      )
+        .then((response) => {
+          this.$store.commit("setCreateServerInfo", null);
+          console.log(response);
+          this.$router.push({ path: "/dashboard" });
+          this.$notify({
+            type: "success",
+            title: `Server ${this.serverName} correctly created`,
           });
-    }
-  } 
-}
+        })
+        .catch((e) => {
+          this.$store.commit("setCreateServerInfo", null);
+          this.$notify({
+            type: "danger",
+            title: `Something went wrong: ${e._message.message}`,
+          });
+        });
+    },
+  },
+};
 </script>
